7aa2758dfb919d1f9d64e95078bc5d7dfa94c8c9
ZION0TECH <vjesuniyi@gmail.com>
2023-05-18 20:46:22 -0700

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints out a string to stdout.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
