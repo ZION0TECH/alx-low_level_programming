68567da6541f284364e87896465abc1ddbd2d89b
ZION0TECH <vjesuniyi@gmail.com>
2023-05-18 19:18:18 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,11 @@
 
 /**
  * main - Entry point for the program
- * 
+ *
  * Return: 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	
+
 	return (0);
-}	
