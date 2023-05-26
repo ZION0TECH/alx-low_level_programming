accafaf7198c8f4f181d0a65ae0cde0c192701fa
ZION0TECH <vjesuniyi@gmail.com>
2023-05-25 20:43:26 -0700

    mission accomplished

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
+#include "main.h"
 
-void positive_or_negative(int i)
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
 {
+int i;
 
-if (i > 0)
-printf("%d is positive\n", i);
+i = 0;
+positive_or_negative(i);
 
-else if (i < 0)
-printf("%d is negative\n", i);
-
-else
-printf("%d is zero\n", i);
-
-
+return (0);
 }
--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
-/**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
-
-int main(void)
+void positive_or_negative(int i)
 {
-int i;
 
-i = 0;
-positive_or_negative(i);
+if (i > 0)
+printf("%d is positive\n", i);
 
-return (0);
+else if (i < 0)
+printf("%d is negative\n", i);
+
+else
+printf("%d is zero\n", i);
+
 }
